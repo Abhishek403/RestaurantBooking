77cfaf65a496fbfc83021d9e1d58d857357fdaa6
Vaibhav, Kumar <VaibhavK@advisory.com>
2015-02-15 21:15:45 +0530

    adding state and city static tables

--- SqlScripts/CreateSchema.sql
+++ SqlScripts/CreateSchema.sql
@@ -1,21 +1,51 @@
 IF  NOT EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'[States]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [States]
+(
+	[StateId]  int identity(1,1) not null,
+	[StateName] varchar(100) not null
+)
+ALTER TABLE [States]
+ 	ADD CONSTRAINT [XPKStates] PRIMARY KEY  CLUSTERED ([StateId] ASC)
+ END
+
+IF  NOT EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'[Cities]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [Cities]
+(
+	[CityId]  int identity(1,1) not null,
+	[CityName] varchar(100) not null,
+	[StateId] int not null
+)
+ALTER TABLE [Cities]
+ 	ADD CONSTRAINT [XPKCities] PRIMARY KEY  CLUSTERED ([CityId] ASC)
+ END
+
+IF  NOT EXISTS (SELECT * FROM sys.objects 
 WHERE object_id = OBJECT_ID(N'[Restaurants]') AND type in (N'U'))
 BEGIN
 CREATE TABLE [Restaurants]
 ( 
-	[RestaurantsId]	
-	[NatureOfRelationShip] varchar(255)  NULL ,
-	[Name]               varchar(255)  NULL ,
-	[JobTitle]           varchar(255)  NULL ,
-	[Address]            varchar(MAX)  NULL ,
+	[RestaurantId]	   int identity(1,1) NOT NULL,
+	[RestName]               varchar(255)  NOT NULL ,
+	[PinCode]           varchar(10)  NULL ,
+	[StateId]           int  NOT NULL ,
+	[Address]            varchar(500)  NULL ,
 	[Mobile]             varchar(20)  NULL ,
-	[BusinessPhoneNo]    varchar(20)  NULL ,
-	[AcquaintanceId]     int identity(1,1)  NOT NULL 
+	[BusinessPhoneNo]    varchar(20)  NULL,
+	[CityId]			int not null,
+	[TimeSlotTypeId]	int not null,
+	CONSTRAINT fk1_child FOREIGN KEY (parent_id)
+      REFERENCES parent (parent_id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
 )
+ ALTER TABLE [Restaurants]
+ 	ADD CONSTRAINT [XPKRestaurants] PRIMARY KEY  CLUSTERED ([RestaurantId] ASC)
+ END
 
--- ALTER TABLE [Acquaintances]
--- 	ADD CONSTRAINT [XPKAcquaintance] PRIMARY KEY  CLUSTERED ([AcquaintanceId] ASC)
--- END
+
 
 -- ALTER TABLE [Referrers]
 -- 	ADD CONSTRAINT [R_11] FOREIGN KEY ([HospitalEmpanelledWith]) REFERENCES [Hospitals]([HospitalId])
--- SqlScripts/DropSchema.sql
+++ SqlScripts/DropSchema.sql
@@ -3,3 +3,15 @@ WHERE object_id = OBJECT_ID(N'Restaurants') AND type in (N'U'))
 BEGIN
 drop table Restaurants
 END
+
+IF  EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'Cities') AND type in (N'U'))
+BEGIN
+drop table Cities
+END
+
+IF  EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'States') AND type in (N'U'))
+BEGIN
+drop table States
+END
